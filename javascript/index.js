96876608e21cef3ab497642c7225c51617f36696
L3gend-kmh <kmh14official@gmail.com>
2022-11-12 23:04:29 +0630

    main

--- javascript/index.js
+++ javascript/index.js
@@ -17,6 +17,7 @@ function login() {
 }
 
 function pswcheck() {
+    
     if (/[A-Z]/g.test(registerform.psw1.value)) {
         document.getElementById("pswerror").innerHTML = "";
     } else {
@@ -54,16 +55,16 @@ function pswcheck() {
 }
 
 var sidemenubox = document.getElementById('menuitems');
-var icon = document.getElementById('bars');
+var menueicon = document.getElementById('bars');
 function sidemenu() {
     if (document.getElementById('menucheck').checked == true) {
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-xmark');
+        menueicon.classList.remove('fa-bars');
+        menueicon.classList.add('fa-xmark');
         sidemenubox.style.width = "140px";
         document.getElementById('login-form').style.display = 'none';
     } else {
-        icon.classList.remove('fa-xmark');
-        icon.classList.add('fa-bars');
+        menueicon.classList.remove('fa-xmark');
+        menueicon.classList.add('fa-bars');
         sidemenubox.style.width = "0";
     }
 }
@@ -73,8 +74,8 @@ myFunction(w)
 w.addListener(myFunction)
 function myFunction(w) {
     if (w.matches) {
-        icon.classList.remove('fa-xmark');
-        icon.classList.add('fa-bars');
+        menueicon.classList.remove('fa-xmark');
+        menueicon.classList.add('fa-bars');
         document.getElementById('menucheck').checked = false;
     } else {
         sidemenubox.style.width = "100%";
@@ -94,5 +95,60 @@ function showhide() {
         document.getElementById('pswshow').classList.remove('fa-solid', 'fa-eye');
         document.getElementById('pswshow').classList.add('fa-solid', 'fa-eye-slash');
         document.getElementById('pswshow').style.color = 'red';
+    }
+}
+var cardtype=document.getElementById('cardnumber');
+var cardicon =document.getElementById('card-logo');
+
+function checkcardtype(){
+    
+    if (/^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/g.test(cardtype.value)) {
+        cardicon.classList.add('fa-brands','fa-cc-mastercard');
+        cardicon.style.color="green";
+        return true;
+    } else {
+        cardicon.classList.remove('fa-brands','fa-cc-mastercard');
+        cardicon.classList.add('fa-regular','fa-credit-card');
+        cardicon.style.color="red";
+    }
+
+    if (/3[47][0-9]{13}$/g.test(cardtype.value)) {
+        cardicon.classList.add('fa-brands','fa-cc-amex');
+        cardicon.style.color="green";
+        return true;
+    } else {
+        cardicon.classList.remove('fa-brands','fa-cc-amex');
+        cardicon.classList.add('fa-regular','fa-credit-card');
+        cardicon.style.color="red";
+    }
+    
+    if (/(4\d{3})((-\d{4}){3}|(\s\d{4}){3}|\d{12})/g.test(cardtype.value)) {
+        cardicon.classList.add('fa-brands','fa-cc-visa');
+        cardicon.style.color="green";
+        return true;
+    } else {
+        cardicon.style.color="red";
+        cardicon.classList.remove('fa-brands','fa-cc-visa');
+        cardicon.classList.add('fa-regular','fa-credit-card');
+    }
+    
+    if (/^(?:2131|1800|35[0-9]{3})[0-9]{11}$/g.test(cardtype.value)) {
+        cardicon.classList.add('fa-brands','fa-cc-jcb');
+        cardicon.style.color="green";
+        return true;
+    } else {
+        cardicon.classList.remove('fa-brands','fa-cc-jcb');
+        cardicon.classList.add('fa-regular','fa-credit-card');
+        cardicon.style.color="red";
+    }
+
+    if (/^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$/g.test(cardtype.value)) {
+        cardicon.classList.add('fa-brands','fa-cc-discover');
+        cardicon.style.color="green";
+        return true;
+    } else {
+        cardicon.classList.remove('fa-brands','fa-cc-discover');
+        cardicon.classList.add('fa-regular','fa-credit-card');
+        cardicon.style.color="red";
     }
 }
